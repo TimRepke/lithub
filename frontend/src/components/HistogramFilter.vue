1362007e4933a58b12113471c83ed9fbccf2cb98
Tim Repke <repke@mcc-berlin.net>
2024-04-30 19:59:42 +0200

    fix most histogram glitches

--- frontend/src/components/HistogramFilter.vue
+++ frontend/src/components/HistogramFilter.vue
@@ -21,7 +21,7 @@ const colours = {
   barTotal: "#dadacd",
   barFiltered: "#e6ab02",
 };
-type Stack = { year: number; count: number; colour: string };
+type Stack = { year: number; count: number; colour: string; padding: number };
 type Year = { year: number; stack: Stack[] };
 const data = computed(() =>
   years.map((yr) => ({
@@ -31,25 +31,45 @@ const data = computed(() =>
         year: yr,
         count: extent.value.total[1],
         colour: "white",
+        padding: 1,
       },
       {
         year: yr,
         count: masks[yr].counts.value.countTotal,
         colour: colours.barTotal,
+        padding: 0,
       } as Stack,
       {
         year: yr,
         count: masks[yr].counts.value.countFiltered,
         colour: colours.barFiltered,
+        padding: 0,
       } as Stack,
     ],
   })),
 );
 
-//  + margin.left + margin.right, size.h + margin.top + margin.bottom
-const svg = d3create("svg");
-// .attr("viewBox", [0, 0, width.value, height.value]);
-// .on("click", resetZoom);
+let focusYear: number | null = null;
+
+function showTooltip(d: Year) {
+  tooltip
+    .html(`<strong>${d.year}</strong><br />Total: ${d.stack[1].count}<br />Filtered: ${d.stack[2].count}`)
+    .classed("hidden", false)
+    .style("top", "10px");
+
+  const offset =
+    d.year > middle ? -(tooltip.node()?.getBoundingClientRect().width ?? 100) - xScale.step() / 2 : xScale.step();
+  tooltip.style("left", `${(xScale(d.year) ?? 0) + offset}px`);
+
+  groupBars.selectAll("rect.bar").classed("hist-hl", false);
+  (groupBars.select(`g.barstack[year="${d.year}"]`).node() as SVGGElement).children[0].classList.add("hist-hl");
+  clearTooltipDelay();
+}
+
+const { delayedCall: hideTooltip, clear: clearTooltipDelay } = useDelay(() => {
+  groupBars.selectAll("rect.bar").classed("hist-hl", false);
+  tooltip.classed("hidden", true);
+}, 300);
 
 const yScale = scaleLinear().domain(extent.value.total);
 const xScale = scaleBand<number>() //
@@ -57,33 +77,44 @@ const xScale = scaleBand<number>() //
   .padding(0.2);
 const xAxis = axisBottom<number>(xScale);
 
-const g = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
-const groupAxis = g.append("g").attr("transform", `translate(0, ${height.value - 5})`);
-const groupBars = g.append("g").attr("transform", `translate(0, -6)`);
-const groupBrush = g.append("g").attr("class", "brush");
-
 const tooltip = d3create("div").attr("class", "hist-tooltip hidden");
+const svg = d3create("svg");
+const g = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
+const groupBars = g.append("g").attr("transform", `translate(0, -5)`);
+const groupAxis = g.append("g").attr("transform", `translate(0, ${height.value - 5})`);
+const groupBrush = g
+  .append("g")
+  .attr("class", "brush")
+  .on("wheel", (e) => e.preventDefault())
+  .on("mousemove", (e: MouseEvent) => {
+    const hoverYear = Math.max(0, Math.min(years.length - 1, Math.floor(e.clientX / xScale.step()) - 1));
+    if (hoverYear !== focusYear) {
+      focusYear = hoverYear;
+      showTooltip(data.value[focusYear]);
+    }
+  })
+  .on("mouseleave", hideTooltip);
 
-svg.call(
-  d3zoom<SVGSVGElement, undefined>()
-    .scaleExtent([1, 100])
-    .translateExtent([
-      [0, 0],
-      [width.value, height.value],
-    ])
-    .extent([
-      [0, 0],
-      [width.value, height.value],
-    ])
-    .on("zoom", (event: D3ZoomEvent<SVGSVGElement, undefined>) => {
-      groupBars.attr(
-        "transform",
-        // -6 to account for margin + axis; the rest term adjusts weird offset side effects
-        `translate(0, ${-(height.value * (event.transform.k - 1)) - 6 + (6 * (event.transform.k / 10))})
+const zoom = d3zoom<SVGSVGElement, undefined>()
+  .scaleExtent([1, 100])
+  .translateExtent([
+    [0, 0],
+    [width.value, height.value],
+  ])
+  .extent([
+    [0, 0],
+    [width.value, height.value],
+  ])
+  .on("zoom", (event: D3ZoomEvent<SVGSVGElement, undefined>) => {
+    if (event.sourceEvent && event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+    groupBars.attr(
+      "transform",
+      // -5 to account for margin + axis; the rest term adjusts weird offset side effects
+      `translate(0, ${-(height.value * (event.transform.k - 1)) - 5 + 5 * (event.transform.k / 10)})
                scale(1, ${event.transform.k})`,
-      );
-    }),
-);
+    );
+  });
+
 const brush = brushX<undefined>()
   // .handleSize(8)
   .extent([
@@ -91,6 +122,10 @@ const brush = brushX<undefined>()
     [width.value, height.value],
   ])
   .on("start brush end", (event: D3BrushEvent<undefined>) => {
+    event.sourceEvent.preventDefault();
+    event.sourceEvent.stopPropagation();
+    if (event.sourceEvent && event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+    // var s = d3.event.selection || x2.range();
     if (event.type === "end") {
       if (!event.selection) {
         clear();
@@ -99,8 +134,8 @@ const brush = brushX<undefined>()
         const domain = xScale
           .domain()
           .slice(
-            Math.ceil(Math.max(0, x0 - (xScale.step() / 2)) / xScale.step()),
-            Math.floor(Math.min(width.value, x1 + (xScale.step() / 2)) / xScale.step()),
+            Math.ceil(Math.max(0, x0 - xScale.step() / 2) / xScale.step()),
+            Math.floor(Math.min(width.value, x1 + xScale.step() / 2) / xScale.step()),
           );
 
         if (domain.length === 0) {
@@ -119,6 +154,7 @@ const { call: delayedRedraw } = useDelay(() => {
   svg //
     .attr("width", width.value + margin.left + margin.right)
     .attr("height", height.value + margin.top + margin.bottom);
+  // groupZoom.attr("width", width.value).attr("height", height.value);
   yScale.range([0, height.value]);
   xScale.range([0, width.value]);
   brush.extent([
@@ -126,9 +162,8 @@ const { call: delayedRedraw } = useDelay(() => {
     [width.value, height.value],
   ]);
 
-  const numTicks = Math.floor(Math.max(16 - (width.value / 50), 2));
-  //@ts-ignore
-  xAxis.scale(xScale).tickValues(xScale.domain().filter((y, i) => !(i % numTicks)));
+  const numTicks = Math.floor(Math.max(16 - width.value / 50, 2));
+  xAxis.scale(xScale).tickValues(xScale.domain().filter((_y, i) => !(i % numTicks)));
 
   // Bars
   groupBars
@@ -136,22 +171,21 @@ const { call: delayedRedraw } = useDelay(() => {
     .data(data.value)
     .join("g")
     .attr("class", "barstack")
-    .on("mouseover", showTooltip)
-    //.on("mousemove", mousemove)
-    .on("mouseleave", hideTooltip)
+    .attr("year", (d) => d.year)
     .selectAll("rect")
     .data((d) => d.stack)
     .join("rect")
     .attr("class", "bar")
-    .attr("x", (d) => xScale(d.year) ?? 0)
+    .attr("x", (d) => (xScale(d.year) ?? 0) - d.padding)
     .attr("y", (d) => yScale(extent.value.total[1] - d.count))
-    .attr("width", xScale.bandwidth())
+    .attr("width", (d) => xScale.bandwidth() + d.padding * 2)
     .attr("height", (d) => Math.max(0, yScale(d.count)))
     .attr("fill", (d) => d.colour)
     .attr("opacity", 1);
 
   groupAxis.call(xAxis);
-  groupBrush.call(brush);
+  groupBrush.call(brush); //.call(brush.move, xScale.range());
+  // svg.call(zoom);
 }, 50);
 
 const histogramElement = ref<HTMLDivElement | null>(null);
@@ -169,25 +203,7 @@ onMounted(async () => {
   }
 });
 
-function showTooltip(event: MouseEvent, d: Year) {
-  const offset = d.year > middle ? -100 : 15;
-  tooltip
-    .html(`<strong>${d.year}</strong><br />Total: ${d.stack[1].count}<br />Filtered: ${d.stack[2].count}`)
-    .classed("hidden", false)
-    .style("top", "10px")
-    .style("left", `${(xScale(d.year) ?? 0) + offset}px`);
-  groupBars.selectAll("rect.bar").classed("hist-hl", false);
-  (event.target as SVGRectElement).parentElement?.children[0].classList.add("hist-hl");
-  event.stopPropagation();
-  clearTooltipDelay();
-}
-
-const { delayedCall: hideTooltip, clear: clearTooltipDelay } = useDelay((event: MouseEvent) => {
-  event.stopPropagation();
-  groupBars.selectAll("rect.bar").classed("hist-hl", false);
-  tooltip.classed("hidden", true);
-}, 300);
-
+svg.call(zoom);
 watch([data, width], delayedRedraw);
 </script>
 
@@ -220,7 +236,7 @@ watch([data, width], delayedRedraw);
 .hist-tooltip {
   position: absolute;
   display: block;
-  background-color: white;
+  background-color: #ffffffc4;
   border: 1px solid black;
   border-radius: 5px;
   padding: 10px;
@@ -236,5 +252,12 @@ watch([data, width], delayedRedraw);
 
 .hist-hl {
   fill: #f8ede2;
+  stroke: #5b4b4b;
+}
+
+.zoom {
+  cursor: move;
+  fill: none;
+  pointer-events: all;
 }
 </style>
